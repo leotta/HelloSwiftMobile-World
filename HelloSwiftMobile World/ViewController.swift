2a1ad57dba688db85ccb29c8ca32a52f6ff1cba3
Sinetskiy <sinetskiy@gmail.com>
2016-04-02 14:44:07 +0300

    change digits to worlds

--- HelloSwiftMobile World/ViewController.swift
+++ HelloSwiftMobile World/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
+
+//AutoLayout - позволяет устанавливать правила расположения элементов относительно друг друга
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    @IBOutlet weak var inputField: UITextField!
+
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        let userName = inputField.text ?? "Аноним"
+        let greeting = userName + ", Добро пожаловать в мир разработки мобильных приложений! Ихааа!!!"
+        greetingLabel.text = greeting
+    }
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+//    класс вью контроллера стал дилегатом поля для ввода текста
+    
+        inputField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
+//  поддержка протокола
 
+extension ViewController:UITextFieldDelegate {
+    
+//    скрываем клавиатуру после нажатия ввод
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+//        перестань быть ответсвенным
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        var applyiedString = (textField.text as? NSString)?.stringByReplacingCharactersInRange(range, withString: string)
+        let elementsToConvert = ["1": "Один", "2": "два", "3": "три", "4": "четыре", "5": "пять", "6": "шесть", "7": "семь", "8":"восемь", "9":"девять"]
+        
+        for (digit,letters) in elementsToConvert {
+          applyiedString = applyiedString?.stringByReplacingOccurrencesOfString(digit, withString: letters)
+            
+        }
+        textField.text = applyiedString
+        
+        return false
+    }
+    
+    
+    
+}
